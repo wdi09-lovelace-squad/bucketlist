1494d2ae216b2266ae0571aa26486c2688bd2185
kelvinma <me@kelvinma.com>
2015-11-30 11:40:09 -0500

    Added map layer for search results -KM

--- assets/js/bucketlist-main.js
+++ assets/js/bucketlist-main.js
@@ -14,9 +14,30 @@ $(document).ready(function() {
       data: JSON.stringify(searchParams),
       dataType: 'json'
     }).done(function(data){
+      foursquarePlaces.clearLayers();
       console.log(data.response);
+      searchLayer(data);
     }).fail(function(jqxhr) {
       console.error(jqxhr);
     });
   });
+
+  var foursquarePlaces = L.layerGroup().addTo(map);
+
+  var searchLayer = function(data) {
+    for (var i = 0; i < data.response.venues.length; i++) {
+      var venue = data.response.venues[i];
+      var latlng = L.latLng(venue.location.lat, venue.location.lng);
+      var marker = L.marker(latlng, {
+          icon: L.mapbox.marker.icon({
+            'marker-color': '#3bb2d0',
+            'marker-size': 'small'
+          })
+        })
+      .bindPopup('<strong><a href="https://foursquare.com/v/' + venue.id + '">' +
+        venue.name + '</a></strong><br>' + venue.categories[0].name)
+        .addTo(foursquarePlaces);
+    }
+  };
+
 });
